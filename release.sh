90ddeb97b9e6588e68908d78d25b4f6dad364674
toto240325 <toto240325@gmail.com>
2021-11-12 08:53:35 +0100

    .

--- release.sh
+++ release.sh
@@ -25,19 +25,30 @@ git checkout `cat version.tmp`
 
 # this should be done by ansible only once
 # sudo mkdir -p /var/www/event
+# sudo cp -v params.php /var/www/event/
+# scp /opt/db/mydatabase.db sd8:/tmp/
+# ssh sd8 "sudo mkdir -p /opt/db"
+# ssh sd8 "sudo cp -v /tmp/mydatabase.db /opt/db/"
 
-sudo mkdir -p /var/www/event/
-sudo cp -rv {api,config,models,version.tmp} /var/www/event/
-sudo cp -v params.php /var/www/event/
-sudo vi /var/www/event/params.php
+
+
+ssh sd8 "sudo mkdir -p /var/www/event/"
+ssh sd8 "sudo git clone --depth 1 https://github.com/toto240325/event.git"
+
+
+
+scp -v params.php sd8:/var/www/event/
+scp -rv {api,config,models,version.tmp} sd8:/var/www/event/
+#sudo cp -v params.php /var/www/event/
+#sudo vi /var/www/event/params.php
 
 rm version.tmp
 
 # checkout master again (current version is not master)
 git checkout master
 
-sudo su
-. /opt/venv/watchdog_prod/bin/activate
-cd /opt/watchdog
-pip install -r requirements.txt
-python watchdog.py
+# sudo su
+# . /opt/venv/watchdog_prod/bin/activate
+# cd /opt/watchdog
+# pip install -r requirements.txt
+# python watchdog.py
